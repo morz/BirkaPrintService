7b2a69635c12bf17df76ad9ed42a4db6023afb09
Данил <dr.morz@gmail.com>
2013-04-18 19:49:10 +0400

    add service & service installer

---
+++ BirkaPrintService/BirkaPrintService.cs
@@ -0,0 +1,38 @@
+﻿using Bend.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    partial class BirkaPrintService : ServiceBase
+    {
+        public BirkaPrintService()
+        {
+            InitializeComponent();
+            new ServiceStarter();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public static void Main()
+        {
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+            //Change the following line to match. 
+            ServicesToRun = new System.ServiceProcess.ServiceBase[] { new BirkaPrintService() };
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
--- BirkaPrintService/Program.cs
+++ BirkaPrintService/Program.cs
@@ -5,44 +5,13 @@ using System.Threading;
 
 namespace ConsoleApplication1
 {
-    public static class NativeMethods
+    public class ServiceStarter
     {
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-    }
-    class Program
-    {
-        
-
-        static public int Main(String[] args)
+        public ServiceStarter()
         {
-            Console.Title = "Сервис печати бирок";
-            IntPtr hWnd = NativeMethods.FindWindow(null, Console.Title);
-
-            if (hWnd != IntPtr.Zero)
-            {
-                NativeMethods.ShowWindow(hWnd, 0); // 0 = SW_HIDE
-            }
-
-            HttpServer httpServer;
-            if (args.GetLength(0) > 0)
-            {
-                httpServer = new MyHttpServer(Convert.ToInt16(args[0]));
-            }
-            else
-            {
-                httpServer = new MyHttpServer(80);
-            }
+            HttpServer httpServer = new MyHttpServer(80);
             Thread thread = new Thread(new ThreadStart(httpServer.listen));
             thread.Start();
-            Console.WriteLine("Сервер запущен на всех IP на этом компьютере.");
-            return 0;
         }
-
-        
-
     }
 }
